96f8452094c27496ea6e3c2812e32158c9b36e8a
Matthew Groth <matthewgroth@matthews-mbp.myfiosgateway.com>
2022-05-27 23:26:53 -0400

    first commit

---
+++ build.gradle.kts
@@ -0,0 +1,10 @@
+dependencies {
+  implementation(projects.kj.kjlib.lang)
+  implementation(projects.kj.kjlib)
+  implementation(projects.kj.async)
+  api(libs.kotlinx.serialization.json)
+}
+
+plugins {
+  kotlin("plugin.serialization") version tomlVersion("kotlin")
+}
