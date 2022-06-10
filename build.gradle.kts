26b99f49e3dffdb1132d96af67438af5b7282515
mgroth0 <mgroth49@gmail.com>
2022-06-10 00:42:48 -0400

    autocommit

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,7 @@ dependencies {
   implementation(projects.kj.kjlib)
   implementation(projects.kj.async)
   api(libs.kotlinx.serialization.json)
+  implementation(projects.kj.reflect)
 }
 
 plugins {
