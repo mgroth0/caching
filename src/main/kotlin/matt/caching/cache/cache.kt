760922867482b37080b58f678bc76962d3c67af4
mgroth0 <mgroth49@gmail.com>
2023-02-05 20:13:47 -0500

    autocommit

--- src/main/kotlin/matt/caching/cache/cache.kt
+++ src/main/kotlin/matt/caching/cache/cache.kt
@@ -37,10 +37,7 @@ fun <R> Any.permaStaticCache(key: String, op: ()->R?): R? {
 
 
 class LRUCache<K, V>(private val cacheSize: Int): LinkedHashMap<K, V>(16, 0.75f, true) {
-
-  override fun removeEldestEntry(eldest: Map.Entry<K, V>): Boolean {
-	return size >= cacheSize
-  }
+  override fun removeEldestEntry(eldest: Map.Entry<K, V>) = size >= cacheSize
 }
 
 class AssertCache<K, V>(private val cacheSize: Int): LinkedHashMap<K, V>(16, 0.75f, true) {
