4f8447e85bcdff7d17d8ffd7b92125d098eb58b3
mgroth0 <mgroth49@gmail.com>
2023-08-07 01:16:04 -0400

    added src/test/kotlin/matt/caching/test/test.kt,changed 2 files

---
+++ src/test/kotlin/matt/caching/test/test.kt
@@ -0,0 +1,20 @@
+package matt.caching.test
+
+
+import matt.caching.cache.LRUCache
+import matt.test.JupiterTestAssertions.assertRunsInOneSecond
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class CachingTests {
+
+    @Test
+    fun lruCacheDoesRemove() = assertRunsInOneSecond {
+        val size = 10
+        val cache = LRUCache<String, Int>(10)
+        repeat(size + 1) {
+            cache[it.toString()] = it
+        }
+        assertEquals(cache.size, size - 1) /*needs space for quickly adding 1 more?*/
+    }
+}
